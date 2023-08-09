476674098810a4eb3b5ba2b27b851ad4948b0ebd
mac <mac@macs-MacBook-Pro.local>
2023-08-09 10:39:35 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// if (7 < 5) {
+//     console.log('7 is less than 5')
+// } else {
+//     console.log('The condition is false')
+// }
+var myName = "Balogun Oluwatobiloba"
+console.log(myName)
+myName = "Jane Tope"
+console.log(myName)
